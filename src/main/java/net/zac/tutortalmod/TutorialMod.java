1cf679e45bac455ed6b2074c2f96a5366334648c
OEM <zac12hary@gmail.com>
2024-09-14 18:15:42 +1200

    added important comment

--- src/main/java/net/zac/tutortalmod/TutorialMod.java
+++ src/main/java/net/zac/tutortalmod/TutorialMod.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-//
+
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
